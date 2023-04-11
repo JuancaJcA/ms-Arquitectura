d634a0c9f1b6e38902c6ea4d63152c29e91bf5fc
JuancaJcA <avilafloresjc@gmail.com>
2023-04-10 22:24:48 -0400

    Config server working

--- ms-config-server/src/main/kotlin/com/example/msconfigserver/MsConfigServerApplication.kt
+++ ms-config-server/src/main/kotlin/com/example/msconfigserver/MsConfigServerApplication.kt
@@ -2,10 +2,12 @@ package com.example.msconfigserver
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.config.server.EnableConfigServer
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableConfigServer
 class MsConfigServerApplication
 
 fun main(args: Array<String>) {
